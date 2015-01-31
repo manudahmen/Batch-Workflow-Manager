51d319e9eda94bb9b1e021498c9076ca1c7605f0
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-01-31 20:48:29 +0100

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,20 +1,52 @@
 package info.emptycanvas.wmb;
-interface Mode
+interface AppListener
 {
   
 }
-class MailListener implements Mode
+class ByMailListener implements AppListener, Runnable
 {
+  public ByMailListener(String username, String password, String host, int port)
+  {
+    configure(String username, String password, String host, int port);
+  }
   public void configure(String username, String password, String host, int port)
   {
     
   }
+  public void run()
+  {
+    while(app.isRunning())
+    {
+      
+    }
+  }
 }
 
-public class App
+public class App implements Runnable
 {
-  
-  public void listen(Mode mode)
+  List<AppListener> listener = new ArrayList<AppListener>();
+  private boolean running = false;
+
+  public App()
   {
+    initApp();
+    running = true;
+  }
+  public void initApp()
+  {
+    
+  }
+  public void listen(AppListener msg)
+  {
+  }
+  public void run()
+  {
+    
+  }
+  public static void main(String [] args)
+  {
+    App app = new App();
+    new Thread(app).start();
+    app.listen(new ByEmailListener()
   }
 }
