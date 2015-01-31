785b97a26196d094a2ca5fac0b542330acc07133
Manuel Dahmen <manuel.dahmen@gmail.com>
2015-01-31 20:22:05 +0100

    Update App.java

--- src/java/main/App.java
+++ src/java/main/App.java
@@ -1,9 +1,15 @@
 package info.emptycanvas.wmb;
 interface Mode
 {
+  
 }
 class MailListener implements Mode
-{}
+{
+  public void configure(String username, String password, String host, int port)
+  {
+    
+  }
+}
 
 public class App
 {
